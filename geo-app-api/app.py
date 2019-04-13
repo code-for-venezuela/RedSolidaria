e91d13765ee5e0314cd82a40596b3f37a506d28c
Edward Raigosa <wenlock@github.com>
2019-04-13 16:57:55 -0700

    get local testing working

--- geo-app-api/app.py
+++ geo-app-api/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask import request
+# importing necessary libraries
+import reverse_geocoder as rg
+import pprint
+import json
+import os
 
 app = Flask(__name__)
+
+# TODO move this to a module
+# TODO write some test cases for this
+def reverseGeocode(coordinates):
+    return rg.search(coordinates)
 
 @app.route("/")
 def index():
@@ -10,6 +21,20 @@ def index():
 def hello():
     return "Hola Code for Venezuela!!!"
 
+# TODO move this to a module
+# example : http://localhost:5000/revgeocode?lat=12&long=12
+@app.route("/revgeocode")
+def revgecode():
+    # Coorinates tuple.Can contain more than one pair.
+    lat = request.args.get('lat')
+    long = request.args.get('long')
+    coordinates = (lat, long)
+    r = reverseGeocode(coordinates)
+    # result is a list containing ordered dictionary.
+    return json.dumps(r)
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    service_port = os.environ['SERVER_PORT']
+    if len(service_port) == 0 :
+        service_port = 5000
+    app.run(debug=True,host='0.0.0.0',port=service_port)
