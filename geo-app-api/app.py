58ecc8bf497151dcaec8b7d712d189bd1a586185
Edward Raigosa <wenlock@github.com>
2019-04-14 14:40:51 -0700

    Integrating the worksheets google api calls
    This integrates work from Guido so we can show a json dump from
    the worksheet

--- geo-app-api/app.py
+++ geo-app-api/app.py
@@ -23,6 +23,30 @@ scopes =  ['https://www.googleapis.com/auth/spreadsheets.readonly']
 token = open('token.pickle', 'rb')
 creds = pickle.load(token)
 
+#extract information from a Google spreadsheet
+def spread_extract(spread_id, sheet_name):
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=spread_id , range= sheet_name).execute()
+    values = result.get('values', [])
+    return values
+
+#convert list extracted from the Google Spreadsheet to a Pandas dataframe
+## Limitations of the function: All blank cells must be filled with a value. Remember that the encoding is UTF-8, so column names in the Pd dataframe require an 'u' prefix: u'Dirección'.
+
+def list_to_df(values):
+    header = values[0]   # Assumes first line is header!
+    data = values[1:]  # Everything else is data.
+    all_data = []
+    for col_id, col_name in enumerate(header):
+        column_data = []
+        for row in values:
+            column_data.append(row[col_id])
+        ds = pd.Series(data=column_data, name=col_name)
+        all_data.append(ds)
+    df = pd.concat(all_data, axis=1)
+    # print header
+    return df
 
 # TODO move this to a module
 # TODO write some test cases for this
@@ -48,6 +72,11 @@ def revgecode():
     r = reverseGeocode(coordinates)
     # result is a list containing ordered dictionary.
     return json.dumps(r)
+
+# add a route to get a list organizations
+@app.route("/listorgs")
+def listorgs():
+    return json.dumps(spread_extract("1-81VwUDn0daYXsjdLEQPxyt4LalQy_u6hRvPT1M4CyA", "mirror"))
 
 if __name__ == "__main__":
     service_port = os.environ['SERVER_PORT']
--- geo-app-api/entrypoint.sh
+++ geo-app-api/entrypoint.sh
@@ -4,17 +4,18 @@
 # we can run this on heroku using containers
 export SERVER_PORT="${PORT:-5000}"
 export GOOGLE_TOKEN_PICKLE="${GOOGLE_TOKEN_PICKLE:-}"
+export GOOGLE_CREDS_JSON="${GOOGLE_CREDS_JSON:-}"
 echo "Arguments are $*"
 echo "Working with ${SERVER_PORT}"
 
 # generate a pickle file
 # we use the environment variable GOOGLE_TOKEN_PICKLE
-cat > ./token.pickle2 << PICKLE
-${GOOGLE_TOKEN_PICKLE}
-PICKLE
-iconv -f ISO-8859-1 -t UTF-8  ./token.pickle2 > ./token.pickle
-rm ./token.pickle2
+
+cat > ./tools/credentials.json << CREDS
+${GOOGLE_CREDS_JSON}
+CREDS
+python ./tools/oauth.py
+
 ls -altr ./token.pickle
-cat ./token.pickle
 echo "Starting up server"
 exec python app.py
