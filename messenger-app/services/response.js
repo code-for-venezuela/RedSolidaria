2049c5dd7856f421dffb2564716010fad8c45837
Jorge Olivares <jolivares@fb.com>
2019-04-13 18:16:47 -0700

    Refactor

--- messenger-app/services/response.js
+++ messenger-app/services/response.js
@@ -1,22 +1,29 @@
-'use strict';
+"use strict";
 
-const
-  person = require('./person');
+const person = require("./person");
 
 module.exports = class Response {
-
   static genQuickReply(text, quickReplies) {
     let response = {
-      "text": text,
-      "quick_replies": []
+      text: text,
+      quick_replies: []
     };
 
     for (let quickReply of quickReplies) {
-      response['quick_replies'].push({
-        "content_type": "text",
-        "title": quickReply['title'],
-        "payload": quickReply['payload']
-      });
+      if (
+        "content_type" in quickReply &&
+        quickReply["content_type"] == "location"
+      ) {
+        response["quick_replies"].push({
+          content_type: "location"
+        });
+      } else {
+        response["quick_replies"].push({
+          content_type: "text",
+          title: quickReply["title"],
+          payload: quickReply["payload"]
+        });
+      }
     }
 
     return response;
@@ -24,60 +31,63 @@ module.exports = class Response {
 
   static genGenericTemplate(image_url, title, subtitle, buttons) {
     let response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": title,
-            "subtitle": subtitle,
-            "image_url": image_url,
-            "buttons": buttons
-          }]
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: title,
+              subtitle: subtitle,
+              image_url: image_url,
+              buttons: buttons
+            }
+          ]
         }
       }
-    }
+    };
 
     return response;
   }
 
-  static genImageTemplate(image_url, title, subtitle = '') {
+  static genImageTemplate(image_url, title, subtitle = "") {
     let response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": title,
-            "subtitle": subtitle,
-            "image_url": image_url
-          }]
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: title,
+              subtitle: subtitle,
+              image_url: image_url
+            }
+          ]
         }
       }
-    }
+    };
 
     return response;
   }
 
   static genButtonTemplate(title, buttons) {
     let response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "button",
-          "text": title,
-          "buttons": buttons
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: title,
+          buttons: buttons
         }
       }
-    }
+    };
 
     return response;
   }
 
-
   static genText(text) {
     let response = {
-      "text": text
+      text: text
     };
 
     return response;
@@ -85,8 +95,8 @@ module.exports = class Response {
 
   static genTextWithPersona(text, persona_id) {
     let response = {
-      "text": text,
-      "persona_id": persona_id
+      text: text,
+      persona_id: persona_id
     };
 
     return response;
@@ -94,32 +104,39 @@ module.exports = class Response {
 
   static genPostbackButton(title, payload) {
     let response = {
-      "type": "postback",
-      "title": title,
-      "payload": payload
-    }
+      type: "postback",
+      title: title,
+      payload: payload
+    };
 
     return response;
   }
 
   static genWebUrlButton(title, url) {
     let response = {
-      "type": "web_url",
-      "title": title,
-      "url": url
-    }
+      type: "web_url",
+      title: title,
+      url: url
+    };
 
     return response;
   }
 
-  static genNuxMessage() {
-    let welcome = this.genText('get_started.welcome');
+  static genNuxMessages() {
+    let welcome = this.genText(
+      "Bienvenido a Diaspora Venezuela, soporte a los inmigrantes"
+    );
 
-    let guide = this.genText('get_started.guidance');
+    let guide = Response.genQuickReply("Indicanos tu ubicación", [
+      {
+        content_type: "location"
+      },
+      {
+        title: "No quiero indicar mi ubicación",
+        payload: "NO_LOCATION"
+      }
+    ]);
 
-    return [
-      welcome,
-      guide
-    ]
+    return [welcome, guide];
   }
-}
+};
