2613735d8614bb64d6875d9f4e39abda8b7a86ea
Edward Raigosa <wenlock@github.com>
2019-04-14 11:42:09 -0700

    Adding generating netrc file

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
+# We can generate a token with these instructions: https://devcenter.heroku.com/articles/authentication
+# heroku auth:token
+#
+function generate_netrc() {
+    echo $HOME
+    cat > $HOME/.netrc << NETRC
+machine api.heroku.com
+  login ${HEROKU_USERNAME}
+    password ${HEROKU_TOKEN}
+NETRC
+
+}
+
 echo "Starting deploy for ${TARGET_DIR}"
 _PWD="$(pwd)"
 cd "${TARGET_DIR}"
 heroku --version
 pwd
+generate_netrc
+cat $HOME/.netrc
