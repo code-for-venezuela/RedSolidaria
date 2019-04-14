ee2759705b09cec23e7d826b467a01722337875a
Edward Raigosa <wenlock@github.com>
2019-04-14 13:18:20 -0700

    adding the python for google sheets

--- geo-app-api/entrypoint.sh
+++ geo-app-api/entrypoint.sh
@@ -3,9 +3,15 @@
 # we use an entrypoint script here with PORT setting so that
 # we can run this on heroku using containers
 export SERVER_PORT="${PORT:-5000}"
+export GOOGLE_TOKEN_PICKLE="${GOOGLE_TOKEN_PICKLE:-}"
 echo "Arguments are $*"
 echo "Working with ${SERVER_PORT}"
 
-
+# generate a pickle file
+# we use the environment variable GOOGLE_TOKEN_PICKLE
+cat > ./token.pickle << PICKLE
+${GOOGLE_TOKEN_PICKLE}
+PICKLE
+ls -altr ./token.pickle
 echo "Starting up server"
 exec python app.py
